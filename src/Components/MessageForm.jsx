0b31a6d6ce28dc81b496255ce55b8fc5ed361fcd
chris-bacon <chrisbacon2009@hotmail.com>
2017-12-22 21:56:52 +0000

    Removed redundant component imports

--- src/Components/MessageForm.jsx
+++ src/Components/MessageForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const MessageForm = ({handleMessageChange, handleSubmit, message}) => {
     return (
--- src/Components/WelcomeMessage.jsx
+++ src/Components/WelcomeMessage.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const WelcomeMessage = (props) => <h3>Welcome {props.username}</h3>
 
