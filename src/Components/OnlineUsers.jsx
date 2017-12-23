a40dfc2398b88b3e19a7ff10fd78caba051cb259
chris-bacon <chrisbacon2009@hotmail.com>
2017-12-23 13:26:27 +0000

    Added TODO message

--- src/Components/OnlineUsers.jsx
+++ src/Components/OnlineUsers.jsx
@@ -4,6 +4,7 @@ const OnlineUsers = (props) => {
     return (
         <div className="OnlineUsers leftline">
             <h3>Online Users</h3>
+            <h3>THIS IS HARDCODED ATM</h3>
             <ul>
                 {props.online.map((x) => <li>{x}</li>)}
             </ul>
